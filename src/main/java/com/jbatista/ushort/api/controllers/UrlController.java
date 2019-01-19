66252866e949d8b22fa7a207308ebd709ae13079
joao <j.batistareis@gmail.com>
2019-01-19 10:49:36 -0300

    url not found error

--- src/main/java/com/jbatista/ushort/api/controllers/UrlController.java
+++ src/main/java/com/jbatista/ushort/api/controllers/UrlController.java
@@ -2,7 +2,11 @@ package com.jbatista.ushort.api.controllers;
 
 import com.jbatista.ushort.api.entities.Address;
 import com.jbatista.ushort.api.services.UrlManager;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +28,13 @@ public class UrlController {
     @RequestMapping(path = "/api/shorten", method = RequestMethod.GET)
     public Address shorten(@RequestParam String url) {
         return urlManager.process(url);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity shortcutNotFound() {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("The requested short URL was not in our database");
     }
 
 }
