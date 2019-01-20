c3df3eec988612738a59bfee8696d6a086037ff7
joao <j.batistareis@gmail.com>
2019-01-19 21:57:24 -0300

    removed heroku fix

--- src/main/java/com/jbatista/ushort/api/controllers/AdmController.java
+++ src/main/java/com/jbatista/ushort/api/controllers/AdmController.java
@@ -33,18 +33,15 @@ public class AdmController {
         administration.saveConfiguration(configuration);
     }
 
-    /*
     @RequestMapping(path = "/api/admin/user", method = RequestMethod.GET)
     public AdmUser getUser() {
         return administration.getAdmUser();
     }
 
-    
     @RequestMapping(path = "/api/admin/user", method = RequestMethod.POST)
     public void saveUser(@RequestBody AdmUser admUser) {
         administration.saveAdmUser(admUser);
     }
-     */
 
     @RequestMapping(path = "/api/admin/url", method = RequestMethod.GET)
     public AddressPage listUrls(@RequestParam int page, @RequestParam int size) {
