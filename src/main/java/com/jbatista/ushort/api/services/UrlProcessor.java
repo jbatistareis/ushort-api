243fdc8809c3bd70b0510fb1d341b896ab9a451b
joao <j.batistareis@gmail.com>
2019-01-19 22:30:44 -0300

    stats fix

--- src/main/java/com/jbatista/ushort/api/services/UrlProcessor.java
+++ src/main/java/com/jbatista/ushort/api/services/UrlProcessor.java
@@ -56,8 +56,10 @@ public class UrlProcessor {
             stats.incrementReducedLetters(Math.max(0, (address.getFullUrl().length() - 5)));
         });
 
-        stats.setAverageUrlSize(stats.getReducedLeters() / stats.getReducedUrls());
-        stats.setReductionPercentage(Math.round(((stats.getReducedLeters() * 100) / ((stats.getReducedLeters() + (5 * stats.getReducedUrls()))))));
+        if (stats.getReducedUrls() > 0) {
+            stats.setAverageUrlSize(stats.getReducedLeters() / stats.getReducedUrls());
+            stats.setReductionPercentage(Math.round(((stats.getReducedLeters() * 100) / ((stats.getReducedLeters() + (5 * stats.getReducedUrls()))))));
+        }
 
         return stats;
     }
